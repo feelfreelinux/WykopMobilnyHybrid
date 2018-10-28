5cb92e965ddcfab681b8129f32a92a0587ca7dd3
Feelfree <rj23@protonmail.com>
2018-10-28 17:15:14 +0100

    feat: Resizable embeds

--- src/components/Embed/index.tsx
+++ src/components/Embed/index.tsx
@@ -1,14 +1,67 @@
 import React, { PureComponent } from 'react'
 import { Embed } from '../../models'
-import { Image, View } from 'react-native'
-import styles from './styles'
+import { Image, View, TouchableOpacity } from 'react-native'
+import styles, { sizedImageStyle } from './styles'
 
-export default class EmbedComponent extends PureComponent<{ embed: Embed }, {}> {
+interface State {
+    width: number
+    height: number
+    isResized: boolean
+    isLoaded: boolean
+}
+
+export default class EmbedComponent extends PureComponent<{ embed: Embed }, State> {
+    constructor(props) {
+        super(props)
+        this.state = {
+            width: 0,
+            height: 0,
+            isLoaded: false,
+            isResized: false,
+        }
+        Image.getSize(this.props.embed.preview, this._handleSizeReceived, () => {})
+    }
+
+
+    _handleSizeReceived = (width, height) => {
+        this.setState({
+            width,
+            height,
+            isLoaded: true,
+        })
+    }
+
     render() {
         return (
             <View style={styles.containerStyle}>
-                <Image source={{uri: this.props.embed.preview }} style={styles.imageStyle} />
+                { this._renderImage() }
             </View>
-        );
+        )
+    }
+
+    _resizeImage = () => {
+        this.setState({
+            isResized: true,
+        })
+    }
+
+    _renderImage() {
+        if (this.state.isLoaded && this.state.isResized) {
+            const style = sizedImageStyle(this.state.height, this.state.width)
+            return (
+                <Image source={{uri: this.props.embed.preview }} style={style.imageStyle} />
+            )
+        } else if (this.state.isLoaded && !this.state.isResized) {
+            const style = sizedImageStyle(300, this.state.width)
+            return (
+                <TouchableOpacity onPress={this._resizeImage}>
+                    <Image source={{uri: this.props.embed.preview }} style={style.imageStyle} />
+                </TouchableOpacity>
+            )  
+        } else {
+            return (
+                <View  style={{ height: 300, width: '100%' }}/>
+            )
+        }
     }
 }
--- src/components/Embed/styles.ts
+++ src/components/Embed/styles.ts
@@ -1,5 +1,14 @@
 import EStyleSheet from 'react-native-extended-stylesheet'
 
+export const sizedImageStyle = (height, width) => {
+    return EStyleSheet.create({
+        imageStyle: {
+            width,
+            height,
+        }
+    })
+}
+
 export default EStyleSheet.create({
     containerStyle: {
         marginTop: 10,
