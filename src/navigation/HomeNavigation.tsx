bb5824074b4e1d11518efa32593908498c75be08
Kroguc <kroguc@gmail.com>
2018-11-02 19:25:48 +0100

    feat: Made appbar prettier

--- src/navigation/HomeNavigation.tsx
+++ src/navigation/HomeNavigation.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react'
 import { createIconSetFromFontello } from '@expo/vector-icons'
-import { View } from 'react-native'
+import { View, Image, TouchableOpacity, StatusBar } from 'react-native'
 import { Font } from 'expo'
 import fontelloConfig from '../../assets/fonts/config.json'
 import { createStackNavigator, createBottomTabNavigator } from 'react-navigation'
 import MikroblogContainer from '../containers/MikroblogContainer'
 import NotImplemented from '../components/NotImplementedPlaceholder'
 import EntryDetailsContainer from '../containers/EntryDetailsContainer'
+import styles from './styles'
 
 const OWMIcons = createIconSetFromFontello(fontelloConfig, 'owmglyphs')
 
@@ -50,7 +51,7 @@ const Tabs = createBottomTabNavigator(
         LinksScreen: {
             navigationOptions: {
                 title: 'Znaleziska',
-                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_links" color={tintColor} size={24} />
+                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_links" color={tintColor} size={22} />
             },
             screen: NotImplemented,
         },
@@ -58,7 +59,7 @@ const Tabs = createBottomTabNavigator(
         MyWykopScreen: {
             navigationOptions: {
                 title: 'Mój Wykop',
-                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_my_wykop" color={tintColor} size={24} />
+                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_my_wykop" color={tintColor} size={22} />
             },
             screen: NotImplemented,
         },
@@ -66,21 +67,21 @@ const Tabs = createBottomTabNavigator(
         MikroblogScreen: {
             navigationOptions: {
                 title: 'Mikroblog',
-                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_mirkoblog" color={tintColor} size={24} />
+                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_mirkoblog" color={tintColor} size={22} />
             },
             screen: MikroblogContainer,
         },
         FavoriteScreen: {
             navigationOptions: {
                 title: 'Ulubione',
-                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_favourite" color={tintColor} size={24} />
+                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_favourite" color={tintColor} size={22} />
             },
             screen: NotImplemented,
         },
         PrivateMessages: {
             navigationOptions: {
                 title: 'Wiadomości',
-                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_messages" color={tintColor} size={24} />
+                tabBarIcon: ({ tintColor }) => <FontIcon name="ic_navi_messages" color={tintColor} size={22} />
             },
             screen: NotImplemented,
         },
@@ -89,28 +90,56 @@ const Tabs = createBottomTabNavigator(
     {
         initialRouteName: 'MikroblogScreen',
         tabBarOptions: {
-            activeTintColor: 'tomato',
+            activeTintColor: '#3c84c1',
             inactiveTintColor: 'gray',
             showLabel: true,
+            tabStyle: { marginTop: 4 },
+            labelStyle: { fontSize: 10, marginTop: -2, marginBottom: 6 }
         },
     }
 )
 
 export default createStackNavigator({
     Home: {
-      screen: Tabs,
-      navigationOptions: {
-        title: 'Mikroblog',
-        headerTitleStyle: { textAlign:'center', alignSelf:'center', flex:1 }
+        screen: Tabs,
+        navigationOptions: {
+            headerLeft: (
+                <TouchableOpacity style={{ marginLeft: 14 }}>
+                    <Image source={{ uri: "https://www.wykop.pl/cdn/c3397992/avatar_def.png" }} style={{ height: 28, width: 28, borderRadius: 40 }} />
+                </TouchableOpacity>
+            ),
+            headerRight: (
+                <View style={{ flex: 1, flexDirection: 'row', }}>
+                    <TouchableOpacity style={{ marginRight: 16 }}>
+                        <FontIcon name="ic_navi_search" color="black" size={24} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ marginRight: 16 }}>
+                        <FontIcon name="ic_refresh" color="black" size={24} />
+                    </TouchableOpacity>
+                </View>
+            ),
+            headerStyle: { height: 48 },
         },
     },
-    
+
     EntryDetails: {
         screen: EntryDetailsContainer,
         navigationOptions: {
-          title: 'Wpis',
-          },
+            title: 'Wpis',
+            headerTitleStyle: { fontSize: 17, marginLeft: 0, fontWeight: 'bold', },
+            headerStyle: { height: 48 },
+            headerRight: (
+                <View style={{ flex: 1, flexDirection: 'row', }}>
+                    <TouchableOpacity style={{ marginRight: 16 }}>
+                        <FontIcon name="ic_navi_search" color="black" size={24} />
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ marginRight: 16 }}>
+                        <FontIcon name="ic_refresh" color="black" size={24} />
+                    </TouchableOpacity>
+                </View>
+            ),
+        },
     }
-  }, {
-    initialRouteName: 'Home'
-  })
+}, {
+        initialRouteName: 'Home'
+    })
