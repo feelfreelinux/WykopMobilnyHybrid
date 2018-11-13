5f0051668e8cc52e89439dc37e9e2454d1c9eaac
xdk78 <xdk78888@gmail.com>
2018-11-13 19:13:17 +0100

    feat: use cached image in embed

--- lib/widgets/embed.dart
+++ lib/widgets/embed.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:owmflutter/models/models.dart';
-import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class EmbedWidget extends StatelessWidget {
   final Embed embed;
@@ -10,9 +11,10 @@ class EmbedWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(top: 4.0),
-      child: Image.network(
-        embed.preview,
-        height: 200.0,
+      child: CachedNetworkImage(
+        placeholder: Image.memory(kTransparentImage),
+        imageUrl: embed.preview,
+        height: 200,
       ),
     );
   }
