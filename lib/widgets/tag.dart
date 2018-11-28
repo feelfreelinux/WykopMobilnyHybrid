f4f8325175d178979409989d6e4d420b2b2a8d99
alufers <alufers@wp.pl>
2018-11-28 19:48:13 +0100

    Added lookup table colors

--- lib/widgets/tag.dart
+++ lib/widgets/tag.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:color/color.dart' as bColor;
 
-// var colorCombinations = <List<Color>>[
-//   [Colors.black, Colors.blue],
-//   [Colors.indigo, Colors.green],
-//   [Colors.deepOrange, Colors.black],
-//   [Colors.purple, Colors.green],
-//   [Colors.red, Colors.grey.shade300]
-// ];
+var colorCombinations = <List<Color>>[
+  [Colors.black, Colors.blue],
+  [Colors.indigo, Colors.green],
+  [Colors.deepOrange, Colors.black],
+  [Colors.purple, Colors.green],
+  [Colors.red, Colors.grey.shade300]
+];
 
 /**
  * Shows a tag in mikroblog text. Each tag has its own colors assigned.
@@ -19,21 +19,12 @@ class TagWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var hc = this.tag.hashCode;
-    // var col = colorCombinations[(hc / 2).floor() % colorCombinations.length];
-    // if(hc % 2 == 1) {
-    //   col = col.reversed.toList();
-    //}
-    var dark = hc % 2 == 1;
-    hc = (hc / 2).floor();
-    var saturation = 50;
-    var luminance = dark ? 30 : 80;
-
-    var colors = <bColor.RgbColor>[
-      bColor.Color.hsl(hc % 360, saturation, luminance).toRgbColor(),
-      bColor.Color.hsl((hc + 120) % 360, saturation, luminance).toRgbColor()
-    ];
     var gradient =
-        colors.map((col) => Color.fromARGB(255, col.r, col.g, col.b)).toList();
+        colorCombinations[(hc / 2).floor() % colorCombinations.length];
+    if (hc % 2 == 1) {
+      gradient = gradient.reversed.toList();
+    }
+
     return Container(
       decoration: BoxDecoration(
           gradient: new LinearGradient(
@@ -44,7 +35,7 @@ class TagWidget extends StatelessWidget {
         padding: const EdgeInsets.all(2.0),
         child: Text('#' + this.tag,
             style: TextStyle(
-                color: dark ? Color.fromARGB(230, 255, 255, 255) : Colors.black,
+                color: Colors.white,
                 fontWeight: FontWeight.bold,
                 fontSize: 12.0)),
       ),
