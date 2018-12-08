c35592b8cb1571bc1c64f3a4edb3f90fc300bdd4
Unknown <kroguc@gmail.com>
2018-12-08 23:28:52 +0100

    fix: Better position of buttons on BottomNavigationBar

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -13,8 +13,8 @@ class _MainScreenState extends State<MainScreen> {
   int _currentIndex = 0;
   final List<Widget> _children = [
     HomeScreen(),
-    MyWykopScreen(),
     MikroblogScreen(),
+    MyWykopScreen(),
     NotificationsScreen(),
   ];
 
@@ -23,24 +23,22 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       body: _children[_currentIndex],
       floatingActionButton: FloatingActionButton(
-        child: Icon(FontAwesomeIcons.pen, size: 20.0,),
+        child: Icon(
+          FontAwesomeIcons.pen,
+          size: 20.0,
+        ),
         onPressed: () {},
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       bottomNavigationBar: BottomAppBar(
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: 12.0,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              _iconButton(FontAwesomeIcons.home, 0, "Strona główna"),
-              _iconButton(FontAwesomeIcons.userTag, 1, "Mój Wykop"),
-              _iconButton(FontAwesomeIcons.solidPlusSquare, 2, "Mikroblog"),
-              _iconButton(FontAwesomeIcons.solidEnvelope, 3, "Powiadomienia"),
-            ],
-          ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            _iconButton(FontAwesomeIcons.home, 0, "Strona główna"),
+            _iconButton(FontAwesomeIcons.solidPlusSquare, 1, "Mikroblog"),
+            _iconButton(FontAwesomeIcons.userTag, 2, "Mój Wykop"),
+            _iconButton(FontAwesomeIcons.solidEnvelope, 3, "Powiadomienia"),
+          ],
         ),
       ),
     );
@@ -48,7 +46,7 @@ class _MainScreenState extends State<MainScreen> {
 
   Widget _iconButton(IconData icon, num index, String tooltip) {
     return IconButton(
-      iconSize: 20.0,
+      iconSize: 22.0,
       icon: Icon(icon),
       color: Color(_currentIndex == index ? 0xff3c84c1 : 0xff666666),
       onPressed: () => this.setState(() {
