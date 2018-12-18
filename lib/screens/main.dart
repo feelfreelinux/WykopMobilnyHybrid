666da0ef1258a594fbefa083ddaa6e50bea70155
xdk78 <xdk78888@gmail.com>
2018-12-18 22:17:26 +0100

    feat: new app bar look

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -24,14 +24,22 @@ class _MainScreenState extends State<MainScreen> {
       resizeToAvoidBottomPadding: false,
       key: OwmKeys.mainScaffoldKey,
       body: _children[_currentIndex],
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Icon(Icons.create, color: Theme.of(context).accentColor),
+        tooltip: "Dodaj nowy",
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
       bottomNavigationBar: BottomAppBar(
+        shape: CircularNotchedRectangle(),
         color: Theme.of(context).primaryColor,
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
             _iconButton(Icons.home, 0, "Strona główna"),
             _iconButton(Icons.add_box, 1, "Mikroblog"),
-            _iconCreateButton(),
             _iconButton(Icons.loyalty, 2, "Mój Wykop"),
             _iconButton(Icons.mail, 3, "Powiadomienia"),
           ],
@@ -40,43 +48,19 @@ class _MainScreenState extends State<MainScreen> {
     );
   }
 
-  Widget _iconCreateButton() {
-    return Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        Container(
-          width: 36,
-          height: 36,
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: Theme.of(context).textTheme.caption.color.withOpacity(0.3),
-              width: 2.0,
-            ),
-            borderRadius: BorderRadius.circular(100),
-          ),
-        ),
-        IconButton(
-          iconSize: 22.0,
-          icon: Icon(Icons.create),
-          color: Theme.of(context).textTheme.caption.color,
-          onPressed: () {},
-          tooltip: "Dodaj nowy",
-        ),
-      ],
-    );
-  }
-
   Widget _iconButton(IconData icon, num index, String tooltip) {
-    return IconButton(
-      iconSize: 26.0,
-      icon: Icon(icon),
-      color: _currentIndex == index
-          ? Theme.of(context).accentColor
-          : Theme.of(context).textTheme.caption.color,
-      onPressed: () => this.setState(() {
-            this._currentIndex = index;
-          }),
-      tooltip: tooltip,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: IconButton(
+        icon: Icon(icon),
+        color: _currentIndex == index
+            ? Theme.of(context).accentColor
+            : Theme.of(context).textTheme.caption.color,
+        onPressed: () => this.setState(() {
+              this._currentIndex = index;
+            }),
+        tooltip: tooltip,
+      ),
     );
   }
 }
