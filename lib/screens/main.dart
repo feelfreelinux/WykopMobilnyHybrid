9c087598578f1aebf2304b2e1e5f9d59c653e893
Unknown <kroguc@gmail.com>
2018-12-18 21:16:38 +0100

    feat: Better style of navigation bar

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:owmflutter/screens/screens.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:owmflutter/keys.dart';
 
 class MainScreen extends StatefulWidget {
@@ -25,33 +24,55 @@ class _MainScreenState extends State<MainScreen> {
       resizeToAvoidBottomPadding: false,
       key: OwmKeys.mainScaffoldKey,
       body: _children[_currentIndex],
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          FontAwesomeIcons.pen,
-          size: 20.0,
-        ),
-        onPressed: () {},
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
       bottomNavigationBar: BottomAppBar(
+        color: Theme.of(context).primaryColor,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
-            _iconButton(FontAwesomeIcons.home, 0, "Strona główna"),
-            _iconButton(FontAwesomeIcons.solidPlusSquare, 1, "Mikroblog"),
-            _iconButton(FontAwesomeIcons.userTag, 2, "Mój Wykop"),
-            _iconButton(FontAwesomeIcons.solidEnvelope, 3, "Powiadomienia"),
+            _iconButton(Icons.home, 0, "Strona główna"),
+            _iconButton(Icons.add_box, 1, "Mikroblog"),
+            _iconCreateButton(),
+            _iconButton(Icons.loyalty, 2, "Mój Wykop"),
+            _iconButton(Icons.mail, 3, "Powiadomienia"),
           ],
         ),
       ),
     );
   }
 
+  Widget _iconCreateButton() {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        Container(
+          width: 36,
+          height: 36,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Theme.of(context).textTheme.caption.color.withOpacity(0.3),
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(100),
+          ),
+        ),
+        IconButton(
+          iconSize: 22.0,
+          icon: Icon(Icons.create),
+          color: Theme.of(context).textTheme.caption.color,
+          onPressed: () {},
+          tooltip: "Dodaj nowy",
+        ),
+      ],
+    );
+  }
+
   Widget _iconButton(IconData icon, num index, String tooltip) {
     return IconButton(
-      iconSize: 22.0,
+      iconSize: 26.0,
       icon: Icon(icon),
-      color: Color(_currentIndex == index ? 0xff3c84c1 : 0xff666666),
+      color: _currentIndex == index
+          ? Theme.of(context).accentColor
+          : Theme.of(context).textTheme.caption.color,
       onPressed: () => this.setState(() {
             this._currentIndex = index;
           }),
