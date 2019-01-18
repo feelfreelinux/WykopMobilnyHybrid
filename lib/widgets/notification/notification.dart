fa059417111597187f640236f79cb4c121b7119f
Filip <rj23@protonmail.com>
2019-01-18 16:03:49 +0100

    feat: New notification mark, marking notifications as read

--- lib/widgets/notification/notification.dart
+++ lib/widgets/notification/notification.dart
@@ -15,56 +15,103 @@ class NotificationWidget extends StatelessWidget {
     return Padding(
         key: Key(notificationId.toString()),
         padding: EdgeInsets.only(top: 0.0, bottom: 3.0),
-        child: Material(
-            color: Theme.of(context).cardColor,
-            elevation: 0.0,
-            child: StoreConnector<AppState, Models.Notification>(
-                converter: (store) =>
-                    store.state.entitiesState.notifications[notificationId],
-                builder: (context, notification) {
-                  return GestureDetector(
-                    onTap: () {
-                      WykopNavigator.handleUrl(context, notification.url);
-                    },
-                    child: Row(
-                      children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: AvatarWidget(
-                              author: notification.author, size: 40),
-                        ),
-                        new Flexible(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: new Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                RichText(
-                                    softWrap: true,
-                                    text: TextSpan(
-                                      children: [
-                                        TextSpan(
-                                            text: notification.body.substring(0,
-                                                notification.body.indexOf(' ')),
-                                            style: TextStyle(
-                                                color: Utils.getAuthorColor(
-                                                    notification.author,
-                                                    context))),
-                                        TextSpan(
-                                            text: notification.body.substring(
-                                                notification.body.indexOf(' '),
-                                                notification.body.length),
-                                            style:
-                                                TextStyle(color: Theme.of(context).textTheme.headline.color))
-                                      ],
-                                    ))
-                              ],
-                            ),
+        child: StoreConnector<AppState, Models.Notification>(
+            converter: (store) =>
+                store.state.entitiesState.notifications[notificationId],
+            builder: (context, notification) {
+              return Stack(children: [
+                Material(
+                    color: Theme.of(context).cardColor,
+                    elevation: 0.0,
+                    child: StoreConnector<AppState, VoidCallback>(
+                      converter: (store) => () => store
+                          .dispatch(markNotificationAsRead(notification.id)),
+                      builder: (context, markAsRead) => GestureDetector(
+                            onTap: () {
+                              markAsRead();
+                              WykopNavigator.handleUrl(
+                                  context, notification.url);
+                            },
+                            child: Column(children: [
+                              Row(
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: AvatarWidget(
+                                        author: notification.author, size: 40),
+                                  ),
+                                  new Flexible(
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: new Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          RichText(
+                                              softWrap: true,
+                                              text: TextSpan(
+                                                children: [
+                                                  TextSpan(
+                                                      text: notification.body
+                                                          .substring(
+                                                              0,
+                                                              notification.body
+                                                                  .indexOf(
+                                                                      ' ')),
+                                                      style: TextStyle(
+                                                          color: Utils
+                                                              .getAuthorColor(
+                                                                  notification
+                                                                      .author,
+                                                                  context))),
+                                                  TextSpan(
+                                                      text: notification.body
+                                                          .substring(
+                                                              notification.body
+                                                                  .indexOf(' '),
+                                                              notification
+                                                                  .body.length),
+                                                      style: TextStyle(
+                                                          color:
+                                                              Theme.of(context)
+                                                                  .textTheme
+                                                                  .headline
+                                                                  .color))
+                                                ],
+                                              ))
+                                        ],
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 8.0, vertical: 0),
+                                child: Divider(
+                                  height: 1.0,
+                                ),
+                              ),
+                            ]),
                           ),
-                        )
-                      ],
-                    ),
-                  );
-                })));
+                    )),
+                Positioned(
+                  right: 0,
+                  top: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: notification.isNew
+                        ? Container(
+                            width: 8,
+                            height: 8,
+                            decoration: BoxDecoration(
+                                color: Colors.greenAccent,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(8.0))))
+                        : Container(),
+                  ),
+                ),
+              ]);
+            }));
   }
 }
